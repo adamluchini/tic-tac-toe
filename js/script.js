ca405e18b02ccabf24d0160e78dac63e23a868b2
Adam Luchini and Cory Olson <adam@>
2016-06-16 16:40:31 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,45 +1,52 @@
-function Game() {
+function Game(players) {
   this.turn=0;
   this.board = [[0, 0, 0],
                 [0, 0, 0],
                 [0, 0, 0]];
-  this.toggleTurn = function(){
-    (this.turn === 0) ? this.turn += 1 : this.turn = 0;
-  }
-  this.checkWin = function(){
-    var mark = this.turn + 1;
-    var win = false;
-    debugger;
-    for(var i = 0; i < this.board.length; i++){
-      //veritical
-      if(this.board[i][i] === mark && this.board[i+1][i] === mark && this.board[i+2][i] === mark){
-        win = true;
-      }
-      // horizontal
-      if(this.board[i][0] === mark && this.board[i][1] === mark && this.board[i][2] === mark){
-        win = true;
-      }
-      //diagonal (down) starting from top left
-      if(this.board[i][0] === mark && this.board[i+1][1] === mark && this.board[i+2][2]){
-        win = true;
-      }
-      // diagonal (up) starting from bottom left
-      if(this.board[i][2] === mark && this.board[i+1][1] === mark && this.board[i+2][0]){
-        win = true;
-      }
-      return win;
+  this.players = players;
+}
+
+Game.prototype.toggleTurn = function(){
+  (this.turn === 0) ? this.turn += 1 : this.turn = 0;
+}
+
+Game.prototype.checkWin = function(){
+  var mark = this.turn + 1;
+  var win = false;
+  debugger;
+  for(var i = 0; i < this.board.length; i++){
+    //veritical
+    if(this.board[0][i] === mark && this.board[1][i] === mark && this.board[2][i] === mark){
+      win = true;
     }
+    // horizontal
+    if(this.board[i][0] === mark && this.board[i][1] === mark && this.board[i][2] === mark){
+      win = true;
+    }
+    //diagonal (down) starting from top left
+    if(this.board[i][0] === mark && this.board[i+1][1] === mark && this.board[i+2][2]){
+      win = true;
+    }
+    // diagonal (up) starting from bottom left
+    if(this.board[i][2] === mark && this.board[i+1][1] === mark && this.board[i+2][0]){
+      win = true;
+    }
+    return win;
   }
 }
+
 
 
 
 $(function(){
   $('button.board').prop("disabled", true);
   $('#newgame').click(function(event){
+    debugger;
     $('button.board').prop("disabled", false);
     $('#newgame').prop("disabled", true);
-    var game = new Game();
+    $('#players').prop("disabled", true);
+    var game = new Game($('#players').val());
+
     var selection = [];
     $('button.board').click(function(event){
 
@@ -55,6 +62,7 @@ $(function(){
         if (game.checkWin()){
           $('button.board').prop("disabled", true);
           $('#newgame').prop("disabled", false);
+          $('#players').prop("disabled", false);
         }else{
           game.toggleTurn();
         }
